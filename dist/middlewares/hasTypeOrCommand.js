a28b5ae673b5384ec259c86173d95706d66e0938
polabiel <bielgabrieloliveira77@gmail.com>
2023-08-21 13:59:03 -0300

    exclude: 🎉tirando o build;

--- dist/middlewares/hasTypeOrCommand.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function hasTypeOrCommand(type, command) {
-    return !!type && !!command;
-}
-exports.default = hasTypeOrCommand;
