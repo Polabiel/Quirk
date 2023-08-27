c4129a953a3ae92a5303f615c0d054aeac19bc10
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2023-08-27 19:50:33 -0300

    fix: colocando valores no cache.json

--- src/repositories/checkCache.ts
+++ src/repositories/checkCache.ts
@@ -4,6 +4,10 @@ import { general } from "../configuration/general";
 
 const cachePath = path.resolve(general.CACHE_DIR, "cache.json");
 
+if (!fs.existsSync(cachePath)) {
+  fs.writeFileSync(cachePath, "[]");
+}
+
 export default async function (remoteJid: string) {
   const cache: string[] = JSON.parse(fs.readFileSync(cachePath, "utf-8"));
 
