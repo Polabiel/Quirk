071db22c87ec03716b5c97d32e939fc1c1f800e8
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-02-01 23:13:08 -0300

    Update .gitignore and fato.ts

--- src/commands/secure/fato.ts
+++ src/commands/secure/fato.ts
@@ -1,7 +1,7 @@
 import { DangerError } from "../../errors/DangerError";
 import { ICommand } from "../../interfaces/ICommand";
 import { general } from "../../configuration/general";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, fatos } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const command: ICommand = {
@@ -11,25 +11,19 @@ const command: ICommand = {
   usage: `${general.PREFIX}fato <Escreva o novo fato aqui>`,
   handle: async (data) => {
     if (data.args[0]) {
-      try {
-        await prisma.fatos.create({
-          data: {
-            fato: data.args.join(" "),
-            criador: data.participant,
-          },
-        });
-        data.sendSuccessReply(`Fato adicionado com sucesso!`);
-      } catch (error: any) {
-        throw new DangerError(error);
-      }
+      await prisma.fatos.create({
+        data: {
+          fato: data.args.join(" "),
+          criador: data.participant,
+        },
+      });
+      data.sendSuccessReply(`Fato adicionado com sucesso!`);
     } else {
-      try {
-        const fatos = await prisma.fatos.findMany();
-        const fatoChoice = fatos[Math.floor(Math.random() * fatos.length)];
-        data.sendSuccessReply(`Fato: ${fatoChoice.fato}`);
-      } catch (error: any) {
-        throw new DangerError(error);
-      }
+      const fatos: fatos[] = await prisma.fatos.findMany();
+      if (fatos.length === 0)
+        throw new DangerError("Nenhum fato encontrado, adicione um novo fato!");
+      const fatoChoice = fatos[Math.floor(Math.random() * fatos.length)];
+      data.sendSuccessReply(`Fato: ${fatoChoice.fato}`);
     }
   },
 };
