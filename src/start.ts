deecb016dbae5a24900bc25885cf7e2bac22e364
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-04-11 17:59:06 -0300

    Adiciona função de reinicialização automática do aplicativo

--- src/start.ts
+++ src/start.ts
@@ -5,6 +5,29 @@ async function start(): Promise<void> {
   console.log("🤖 Bot Conectado!\n");
   await onMessagesUpsert();
   console.log("💻 Eventos Carregados!\n");
+
+  setTimeout(() => {
+    restart();
+  }, 2 * 60 * 60 * 1000);
+}
+
+function restart(): void {
+  if (!process.env.SQUARECLOUD || !process.env.APP_id) return;
+
+  const options = {
+    method: "POST",
+    headers: { Authorization: process.env.SQUARECLOUD },
+  };
+
+  fetch(
+    `https://api.squarecloud.app/v2/apps/${process.env.APP_id}/restart`,
+    options
+  )
+    .then((response) => {
+      console.log(response);
+      console.log("✅ Notificação enviada!");
+    })
+    .catch((err) => console.error(err));
 }
 
 start();
