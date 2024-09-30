1e7305ea285a3a2fd3a47338895b2694ffbfbbf3
Polabiel <bielgabrieloliveira77@gmail.com>
2024-09-29 22:14:34 -0300

    feat: aprimorar comando de sugestão para incluir tratamento de erros e resposta ao usuário

--- src/commands/member/sugestao.ts
+++ src/commands/member/sugestao.ts
@@ -3,36 +3,47 @@ import { general } from "../../configuration/general";
 import { InvalidParameterError } from "../../errors/InvalidParameterError";
 import { ICommand } from "../../interfaces/ICommand";
 import { PrismaClient } from "@prisma/client";
+import { WarningError } from "../../errors/WarningError";
+import { DangerError } from "../../errors/DangerError";
 const prisma = new PrismaClient();
 
 const command: ICommand = {
   name: "Sugestão",
   description: `Comando para sugestão de melhorias do ${general.BOT_NAME}`,
   commands: ["sugestão", "sugestao", "sugestões", "sugestoes"],
-  usage: `${general.PREFIX}token <token>`,
+  usage: `${general.PREFIX}sugestao <texto>`,
   handle: async (data) => {
     await data.sendWaitReact();
     if (data.args[0]) {
-      const suggestion = await prisma.suggestions.create({
-        data: {
-          sugestao: data.args[0],
-          user: {
-            connectOrCreate: {
-              where: {
-                number: isJidGroup(data.remoteJid!)
-                  ? data.participant!
-                  : data.remoteJid!,
-              },
-              create: {
-                number: isJidGroup(data.remoteJid!)
-                  ? data.participant!
-                  : data.remoteJid!,
-                name: data.nickName!,
+      const suggestion = await prisma.suggestions
+        .create({
+          data: {
+            sugestao: data.args[0],
+            user: {
+              connectOrCreate: {
+                where: {
+                  number: isJidGroup(data.remoteJid!)
+                    ? data.participant!
+                    : data.remoteJid!,
+                },
+                create: {
+                  number: isJidGroup(data.remoteJid!)
+                    ? data.participant!
+                    : data.remoteJid!,
+                  name: data.nickName!,
+                },
               },
             },
           },
-        },
-      });
+        })
+        .then(() => {
+          return data.sendSuccessReply("Sugestão inserida com sucesso!");
+        })
+        .catch((e: any) => {
+          throw new DangerError(
+            "Erro ao tentar inserir a sugestão no banco de dados!"
+          );
+        });
     } else if (!data.args[0]) {
       throw new InvalidParameterError(
         "Você precisa escrever uma sugestão de melhoria!"
