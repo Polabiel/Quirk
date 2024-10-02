ed7fcfea35c5aa86b9853780731ce8ae003a20a5
Polabiel <bielgabrieloliveira77@gmail.com>
2024-10-01 21:23:42 -0300

    feat: adicionar mensagens de boas-vindas para novos grupos e usuários no banco de dados

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,7 @@ import loadCommomFunctions from "../utils/loadCommomFunctions";
 import { PrismaClient } from "@prisma/client";
 import checkCache, { addCache } from "./checkCache";
 import { logger } from "../middlewares/onMessagesUpsert";
+import { general } from "../configuration/general";
 const prisma = new PrismaClient();
 
 export default async function (
@@ -13,26 +14,45 @@ export default async function (
 
   if (isGroup) {
     if (await checkCache(baileysMessage.key.remoteJid!)) return;
-    await prisma.group.upsert({
+
+    const group = await prisma.group.findUnique({
       where: { number: baileysMessage.key.remoteJid! },
-      update: {},
-      create: {
-        number: baileysMessage.key.remoteJid!,
-      },
     });
-    logger.info(`Grupo: ${baileysMessage.key.remoteJid!} adicionado ao banco de dados.`);
+
+    if (!group) {
+      bot.sendMessage(baileysMessage.key.remoteJid!, {
+        text: `🎉 Bem-vindos ao grupo Quirk! 🤖\nEstamos animados em ter vocês aqui! Para começar a explorar tudo, basta digitar */menu*`,
+      });
+
+      await prisma.group.create({
+        data: { number: baileysMessage.key.remoteJid! },
+      });
+    }
+
+    logger.info(
+      `Grupo: ${baileysMessage.key.remoteJid!} adicionado ao banco de dados.`
+    );
     return await addCache(baileysMessage.key.remoteJid!);
   } else if (!isGroup) {
     if (await checkCache(baileysMessage.key.remoteJid!)) return;
-    await prisma.user.upsert({
+
+    const user = await prisma.user.findUnique({
       where: { number: baileysMessage.key.remoteJid! },
-      update: {},
-      create: {
-        number: baileysMessage.key.remoteJid!,
-        name: nickName!,
-      },
     });
-    logger.info(`Usuário: ${baileysMessage.key.remoteJid!} adicionado ao banco de dados.`);
+
+    if (!user) {
+      await bot.sendMessage(baileysMessage.key.remoteJid!, {
+        text: `🤖 ${general.BOT_NAME} te dá as boas-vindas ${nickName}!🎉\nDigite */menu* para começar e explorar todas as funcionalidades que oferecemos.`,
+      });
+
+      await prisma.user.create({
+        data: { number: baileysMessage.key.remoteJid!, name: nickName! },
+      });
+    }
+
+    logger.info(
+      `Usuário: ${baileysMessage.key.remoteJid!} adicionado ao banco de dados.`
+    );
     return await addCache(baileysMessage.key.remoteJid!);
   }
   logger.error("Erro ao adicionar grupo ou usuário ao banco de dados.");
