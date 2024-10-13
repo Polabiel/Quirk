0356ebcc2ad573dabf393b1d07b7b798a2c38a35
Polabiel <bielgabrieloliveira77@gmail.com>
2024-10-13 01:01:15 -0300

    feat: implementar envio de mensagens aleatórias para usuários e grupos

--- src/middlewares/onDateTime.ts
+++ src/middlewares/onDateTime.ts
@@ -1,3 +1,38 @@
-export default async () => {
-    // new feature
-}
+import { WASocket } from "@whiskeysockets/baileys";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async (bot: WASocket) => {
+  const users = await prisma.user.findMany({});
+
+  const groups = await prisma.group.findMany({});
+
+  if (users) {
+    users.map(async (user) => {
+      groups.forEach(async (group) => {
+        const GroupMetadata = await bot.groupMetadata(group.number);
+        if(GroupMetadata.participants.some(participant => participant.id === user.number)) {
+          return;
+        }
+      });
+      bot.sendMessage(user.number, { text: generateRandomMessage() });
+    });
+  }
+
+  if(groups) {
+    groups.map((group) => {
+      bot.sendMessage(group.number, {text: generateRandomMessage()});
+    })
+  }
+};
+
+const generateRandomMessage = () => {
+  const messages = [
+    "Não se esqueça de usar o comando */menu!*",
+    "Use o comando */menu* para ver todas as opções disponíveis.",
+    "*/sticker* é rápido digitando */s*",
+    "Deixe sugestões para os desenvolvedores no */sugestão [mensagem]"
+  ];
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  return messages[randomIndex];
+};
