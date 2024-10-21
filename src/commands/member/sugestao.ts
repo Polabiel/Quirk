63a8956dbff9223d2ebdbd2c16d96424292d8b34
Polabiel <bielgabrieloliveira77@gmail.com>
2024-10-21 14:09:24 -0300

    fix: simplificar validação de sugestões e melhorar o fluxo de resposta

--- src/commands/member/sugestao.ts
+++ src/commands/member/sugestao.ts
@@ -13,7 +13,7 @@ const command: ICommand = {
   usage: `${general.PREFIX}sugestao [texto]`,
   handle: async (data) => {
     await data.sendWaitReact();
-    if (data.args[0] && data.args[0].length > 0 && data.args[0].length < 10) {
+    if (data.args[0].length > 0 && data.args[0].length < 10) {
       await prisma.suggestions
         .create({
           data: {
@@ -35,42 +35,32 @@ const command: ICommand = {
             },
           },
         })
-        .then(() => {
-          return data.sendSuccessReply("Sugestão inserida com sucesso!");
-        })
         .catch((e: any) => {
           throw new DangerError(
             "Erro ao tentar inserir a sugestão no banco de dados!"
           );
         });
-    } else {
-      console.log("aqui");
-      
-      const suggestions = await prisma.suggestions.findMany({});
-
-      console.log(suggestions);
-      
-      if (suggestions.length === 0) {
-        return data.sendWarningReply("Não existem sugestões pendentes!");
-      }
-
-      const users = await prisma.user.findMany({});
-
-      console.log(users);
-      
-
-      let formattedSuggestion = "";
-      
-      suggestions.forEach((suggestion) => {
-        const user = users.find(
-          (user) => user.number === suggestion.userNumber
-        );
-        formattedSuggestion += `👤 Usuário: ${user?.name ?? "Desconhecido"}\n💡 Sugestão: ${suggestion.sugestao}\n\n`;
-      });
-
-      return data.sendSuccessReply(formattedSuggestion);
+      return data.sendSuccessReply("Sugestão inserida com sucesso!");
     }
+    const suggestions = await prisma.suggestions.findMany({});
+
+    if (suggestions.length === 0) {
+      return data.sendWarningReply("Não existem sugestões pendentes!");
+    }
+
+    const users = await prisma.user.findMany({});
+
+    let formattedSuggestion = "";
+
+    suggestions.forEach((suggestion) => {
+      const user = users.find((user) => user.number === suggestion.userNumber);
+      formattedSuggestion += `👤 Usuário: ${
+        user?.name ?? "Desconhecido"
+      }\n💡 Sugestão: ${suggestion.sugestao}\n\n`;
+    });
+
+    return data.sendSuccessReply(formattedSuggestion);
   },
 };
 
-export default command; 
+export default command;
