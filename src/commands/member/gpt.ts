4e55b8213ff504cfca7b015e0a2577503c1d9ade
Polabiel <bielgabrieloliveira77@gmail.com>
2024-11-08 12:55:46 -0300

    feat: aprimorar comando GPT-3 com validação de entrada e simplificação de lógica

--- src/commands/member/gpt.ts
+++ src/commands/member/gpt.ts
@@ -1,36 +1,22 @@
-import { isJidGroup } from "@whiskeysockets/baileys";
 import { general } from "../../configuration/general";
 import { InvalidParameterError } from "../../errors/InvalidParameterError";
 import { WarningError } from "../../errors/WarningError";
 import { ICommand } from "../../interfaces/ICommand";
-import gpt, { getOpenAIResults } from "../../services/gpt";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { getOpenAIResults } from "../../services/gpt";
 
 const command: ICommand = {
   name: "GPT-3",
-  description: "Comando para perguntar algo para o GPT-3",
-  commands: ["gpt", general.BOT_NAME, "chat", "chat-gpt"],
-  usage: `${general.PREFIX}gpt ${general.BOT_NAME} o que é a vida?`,
+  description: "Comando da IA do Quirk",
+  commands: [general.BOT_NAME, "gpt", "chat", "chat-gpt"],
+  usage: `${general.PREFIX}${general.BOT_NAME} ${general.BOT_NAME} o que é a vida?`,
   handle: async (data) => {
     await data.sendWaitReact();
-    if (
-      general.NUMBERS_HOSTS.includes(
-        isJidGroup(data.remoteJid!) ? data.participant! : data.remoteJid!
-      )
-    ) {
-      const responseText = await getOpenAIResults(data.args[0]);
+    if(!data.args[0] || data.args[0].length < 10) throw new InvalidParameterError("Você precisa informar uma pergunta com mais de 10 caracteres");
+    const responseText = await getOpenAIResults(data.args[0]);
 
-      return await data.sendSuccessReply(responseText.result);
-    }
+    if(!responseText) throw new WarningError("Não foi possível obter uma resposta da IA");
 
-    if (data.isGroup) {
-      const responseText = await getOpenAIResults(data.args[0]);
-
-      await data.sendSuccessReply(responseText);
-    } else {
-      throw new WarningError("Este comando só pode ser executado em grupos!");
-    }
+    await data.sendSuccessReply(responseText);
   },
 };
 
