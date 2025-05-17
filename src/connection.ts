5caf1d88810b543e5488b3d8409d1f8c7b3b3c65
Polabiel <bielgabrieloliveira77@gmail.com>
2025-05-17 17:45:33 -0300

    refactor: substituir console.log por logger.info e melhorar tratamento de conexão no bot

--- src/connection.ts
+++ src/connection.ts
@@ -9,10 +9,10 @@ import makeWASocket, {
   isJidStatusBroadcast,
   useMultiFileAuthState,
 } from "baileys";
-import pino from "pino";
 import { general } from "./configuration/general";
 import qrcode from "qrcode-terminal";
 import { logger } from "./utils/logger";
+import fs from "fs"
 
 export const connect: () => Promise<WASocket> = async () => {
 
@@ -24,51 +24,88 @@ export const connect: () => Promise<WASocket> = async () => {
 
   const bot = makeWASocket({
     browser: Browsers.appropriate("Desktop"),
-    logger: pino({
-      level: 'silent',
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-        },
-      },
-    }),
+    logger: logger,
     printQRInTerminal: false,
-    defaultQueryTimeoutMs: 60 * 1000,
+    defaultQueryTimeoutMs: 30 * 1000,
     auth: state,
     shouldIgnoreJid: (jid) => {
       logger.debug(`Ignoring JID: ${jid}`);
       if (process.env.NODE_ENV?.toLocaleLowerCase() === 'development') {
         if (isJidGroup(jid)) {
-          return !general.GROUP_SECURE.includes(jid as any);
+          return !general.GROUP_SECURE.includes(jid);
         }
         return !general.NUMBERS_HOSTS.includes(jid);
       }
-      return isJidBroadcast(jid) || isJidStatusBroadcast(jid);
+      return isJidBroadcast(jid) || isJidStatusBroadcast(jid)
     },
     keepAliveIntervalMs: 30 * 1000,
     markOnlineOnConnect: true,
   });
 
-  bot.ev.on('connection.update', (update) => {
-    const { connection, lastDisconnect, qr} = update;
+  bot.ev.on('connection.update', async (update) => {
+    const { connection, lastDisconnect, qr } = update;
 
     switch (connection) {
       case "close":
-        logger.debug("🔒 Conexão fechada");
-        const shouldReconnect =
-          (lastDisconnect?.error as Boom)?.output?.statusCode !==
-          DisconnectReason.loggedOut;
 
-        if (shouldReconnect) {
-          connect();
+        const statusCode = (lastDisconnect?.error instanceof Boom)
+          ? lastDisconnect.error.output?.statusCode
+          : undefined;
+
+        if (statusCode === DisconnectReason.loggedOut) {
+          logger.error("🔴 Bot desconectado! Por conta disso, vamos remover a pasta de autenticação e você terá que conectar novamente o bot");
+
+          try {
+            await fs.promises.rm('./assets/auth/baileys', { recursive: true, force: true });
+            logger.info("\n\n🗑️✨ Pasta de autenticação removida com sucesso! Faça login novamente para continuar. 🔑📲\n\n");
+          } catch (err) {
+            logger.error("❌🚫 Erro ao remover a pasta de autenticação:", err);
+          }
+        } else {
+          switch (statusCode) {
+            case DisconnectReason.badSession:
+              logger.warn("❌ Sessão inválida!");
+              break;
+            case DisconnectReason.connectionClosed:
+              logger.warn("🔒 Conexão fechada!");
+              break;
+            case DisconnectReason.connectionLost:
+              logger.warn("📡 Conexão perdida!");
+              break;
+            case DisconnectReason.connectionReplaced:
+              logger.warn("♻️ Conexão substituída!");
+              break;
+            case DisconnectReason.multideviceMismatch:
+              logger.warn("📱 Dispositivo incompatível!");
+              break;
+            case DisconnectReason.forbidden:
+              logger.warn("🚫 Conexão proibida!");
+              break;
+            case DisconnectReason.restartRequired:
+              logger.info('\n\n\n🔄 Me reinicie por favor! Digite "npm start" ou caso esteja em modo de desenvolvimento, Digite "npm run dev".\n\n\n');
+              break;
+            case DisconnectReason.unavailableService:
+              logger.warn("⛔ Serviço indisponível!");
+              break;
+          }
+
+          logger.debug("🔒 Conexão fechada");
+          const shouldReconnect =
+            (lastDisconnect?.error as Boom)?.output?.statusCode !==
+            DisconnectReason.loggedOut;
+
+          if (shouldReconnect) {
+            connect();
+          }
+          break;
         }
         break;
       case "open":
         logger.debug("🔥 Bot Conectado");
         break;
       case "connecting":
-        logger.debug('🫸 Conectando o bot, aguarde...')
+        logger.debug("🔄 Conectando...");
+        break;
     }
 
     if (qr !== undefined) {
--- src/start.ts
+++ src/start.ts
@@ -1,33 +1,11 @@
 import onMessagesUpsert from "./middlewares/onMessagesUpsert";
+import { logger } from "./utils/logger";
 require("dotenv").config();
 
 async function start(): Promise<void> {
-  console.clear();
-  console.log("🟡 Iniciando...\n");
+  logger.info("🟡 Iniciando...\n");
   await onMessagesUpsert();
 
-  setTimeout(() => {
-    restart();
-  }, 1 * 60 * 60 * 1000);
-}
-
-function restart(): void {
-  if (!process.env.SQUARECLOUD || !process.env.APP_ID) return;
-
-  const options = {
-    method: "POST",
-    headers: {
-      Authorization: process.env.SQUARECLOUD,
-    },
-  };
-
-  fetch(
-    `https://api.squarecloud.app/v2/apps/${process.env.APP_ID}/restart`,
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
 }
 
 start();
