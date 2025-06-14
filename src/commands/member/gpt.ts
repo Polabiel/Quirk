ce204cf49c3b6614d45d099bac8bce5c9b7de7ca
Polabiel <bielgabrieloliveira77@gmail.com>
2025-06-14 13:55:40 -0300

    feat: refatorar comando GPT-3 para incluir verificação de grupo e token de IA

--- src/commands/member/gpt.ts
+++ src/commands/member/gpt.ts
@@ -1,8 +1,12 @@
+import { isJidGroup } from "baileys";
 import { general } from "../../configuration/general";
 import { InvalidParameterError } from "../../errors/InvalidParameterError";
 import { WarningError } from "../../errors/WarningError";
 import { ICommand } from "../../interfaces/ICommand";
-import { getOpenAIResults } from "../../services/gpt";
+import gpt from "../../services/gpt";
+import PrismaSingleton from "../../utils/PrismaSingleton";
+
+const prisma = PrismaSingleton.getInstance();
 
 const command: ICommand = {
   name: "GPT-3",
@@ -10,18 +14,29 @@ const command: ICommand = {
   commands: [general.BOT_NAME.toLowerCase(), "gpt", "chat", "chat-gpt"],
   usage: `${general.PREFIX}${general.BOT_NAME} ${general.BOT_NAME} o que é a vida?`,
   handle: async (data) => {
-    await data.sendWaitReact();
-    if (!data.argsJoined || data.argsJoined.length < 10)
-      throw new InvalidParameterError(
-        "Você precisa informar uma pergunta com mais de 10 caracteres"
-      );
-    const responseText = await getOpenAIResults(data.argsJoined);
+    if (isJidGroup(data.remoteJid!)) {
+      await data.sendWaitReact();
+      if (!data.argsJoined || data.argsJoined.length < 10)
+        throw new InvalidParameterError(
+          "Você precisa informar uma pergunta com mais de 10 caracteres"
+        );
 
-    if (!responseText)
-      throw new WarningError("Não foi possível obter uma resposta da IA");
+      const group = await prisma.group.findUnique({
+        where: { number: data.remoteJid! },
+        select: { TOKEN_OPEANAI: true },
+      });
 
-    await data.sendSuccessReply(responseText.result);
-  },
+      if(!group?.TOKEN_OPEANAI) throw new WarningError("Grupo não possui token de IA configurado!");
+
+      const responseText = await gpt(data.argsJoined, group.TOKEN_OPEANAI)
+
+      if (!responseText)
+        throw new WarningError("Não foi possível obter uma resposta da IA");
+
+      await data.sendSuccessReply(responseText);
+    }
+    throw new WarningError("Este comando só pode ser usado em grupos!")
+  }
 };
 
 export default command;
